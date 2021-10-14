dba889fd7f9e48962e74bb12b0d1748da44fdc22
Luis Alejandro Graterol <luisgraterol97@gmail.com>
2021-10-14 18:28:13 -0400

    Agregar scroll de productos destacados

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,13 +41,13 @@ export default function Home() {
       </div>
 
       <div className="card-home">
-        <div className="nav scroll-x mb-4">
+        <div className="nav scroll-x mb-4 pr-4">
           <button className="nav-item active">Construcci&oacute;n</button>
           <button className="nav-item">Comida</button>
           <button className="nav-item">Pl&aacute;stico</button>
           <button className="nav-item mr-0">Cart&oacute;n</button>
         </div>
-        <div className="flex scroll-x">
+        <div className="flex scroll-x mb-4">
           <div className="card flex items-center justify-between w-96 min-w-max mr-4">
             <div className="flex flex-col items-start h-full">
               <h2 className="text-2xl font-bold mb-auto">TMA-2 <br />Modular <br />Headphone</h2>
@@ -67,6 +67,152 @@ export default function Home() {
               </a>
             </div>
             <img className="h-48" src="./CO2-Cable.png" alt="" />
+          </div>
+          <div className="card flex items-center justify-between w-96 min-w-max mr-4">
+            <div className="flex flex-col items-start h-full">
+              <h2 className="text-2xl font-bold mb-auto">TMA-2 <br />Modular <br />Headphone</h2>
+              <a href="#" className="nav-link">
+                Shop Now
+                <img className="ml-2" src="./icons/arrow-right.png" alt=">" />
+              </a>
+            </div>
+            <img className="h-48" src="./TMA-2-Modular-Headphone.png" alt="" />
+          </div>
+          <div className="card flex items-center justify-between w-96 min-w-max mr-4">
+            <div className="flex flex-col items-start h-full">
+              <h2 className="text-2xl font-bold mb-auto">CO2 <br />Cable</h2>
+              <a href="#" className="nav-link">
+                Shop Now
+                <img className="ml-2" src="./icons/arrow-right.png" alt=">" />
+              </a>
+            </div>
+            <img className="h-48" src="./CO2-Cable.png" alt="" />
+          </div>
+          <div className="card flex items-center justify-between w-96 min-w-max mr-4">
+            <div className="flex flex-col items-start h-full">
+              <h2 className="text-2xl font-bold mb-auto">TMA-2 <br />Modular <br />Headphone</h2>
+              <a href="#" className="nav-link">
+                Shop Now
+                <img className="ml-2" src="./icons/arrow-right.png" alt=">" />
+              </a>
+            </div>
+            <img className="h-48" src="./TMA-2-Modular-Headphone.png" alt="" />
+          </div>
+          <div className="card flex items-center justify-between w-96 min-w-max mr-4">
+            <div className="flex flex-col items-start h-full">
+              <h2 className="text-2xl font-bold mb-auto">CO2 <br />Cable</h2>
+              <a href="#" className="nav-link">
+                Shop Now
+                <img className="ml-2" src="./icons/arrow-right.png" alt=">" />
+              </a>
+            </div>
+            <img className="h-48" src="./CO2-Cable.png" alt="" />
+          </div>
+          <div className="card flex items-center justify-between w-96 min-w-max mr-4">
+            <div className="flex flex-col items-start h-full">
+              <h2 className="text-2xl font-bold mb-auto">TMA-2 <br />Modular <br />Headphone</h2>
+              <a href="#" className="nav-link">
+                Shop Now
+                <img className="ml-2" src="./icons/arrow-right.png" alt=">" />
+              </a>
+            </div>
+            <img className="h-48" src="./TMA-2-Modular-Headphone.png" alt="" />
+          </div>
+          <div className="card flex items-center justify-between w-96 min-w-max mr-4">
+            <div className="flex flex-col items-start h-full">
+              <h2 className="text-2xl font-bold mb-auto">CO2 <br />Cable</h2>
+              <a href="#" className="nav-link">
+                Shop Now
+                <img className="ml-2" src="./icons/arrow-right.png" alt=">" />
+              </a>
+            </div>
+            <img className="h-48" src="./CO2-Cable.png" alt="" />
+          </div>
+          <div className="card flex items-center justify-between w-96 min-w-max mr-4">
+            <div className="flex flex-col items-start h-full">
+              <h2 className="text-2xl font-bold mb-auto">TMA-2 <br />Modular <br />Headphone</h2>
+              <a href="#" className="nav-link">
+                Shop Now
+                <img className="ml-2" src="./icons/arrow-right.png" alt=">" />
+              </a>
+            </div>
+            <img className="h-48" src="./TMA-2-Modular-Headphone.png" alt="" />
+          </div>
+          <div className="card flex items-center justify-between w-96 min-w-max mr-4">
+            <div className="flex flex-col items-start h-full">
+              <h2 className="text-2xl font-bold mb-auto">CO2 <br />Cable</h2>
+              <a href="#" className="nav-link">
+                Shop Now
+                <img className="ml-2" src="./icons/arrow-right.png" alt=">" />
+              </a>
+            </div>
+            <img className="h-48" src="./CO2-Cable.png" alt="" />
+          </div>
+        </div>
+        <div className="flex justify-between mt-8 mb-2 pr-4">
+          <h3 className="text-lg font-normal pb-2">Productos Destacados</h3>
+          <a className="font-light text-gray-600 pb-2" href="#">Ver Todos</a>
+        </div>
+        <div className="flex scroll-x mb-4">
+          <div className="card flex flex-col items-start w-56 min-w-max mr-4">
+            <img className="h-48" src="./TMA-2-Modular-Headphone.png" alt="" />
+            <h2 className="text-lg text-left font-normal mb-auto">TMA-2 HD Wireless</h2>
+            <h3 className="text-lg text-left font-bold mb-auto">USD 350</h3>
+          </div>
+          <div className="card flex flex-col items-start w-56 min-w-max mr-4">
+            <img className="h-48" src="./CO2-Cable.png" alt="" />
+            <h2 className="text-lg text-left font-normal mb-auto">TMA-2 HD Wireless</h2>
+            <h3 className="text-lg text-left font-bold mb-auto">USD 350</h3>
+          </div>
+          <div className="card flex flex-col items-start w-56 min-w-max mr-4">
+            <img className="h-48" src="./TMA-2-Modular-Headphone.png" alt="" />
+            <h2 className="text-lg text-left font-normal mb-auto">TMA-2 HD Wireless</h2>
+            <h3 className="text-lg text-left font-bold mb-auto">USD 350</h3>
+          </div>
+          <div className="card flex flex-col items-start w-56 min-w-max mr-4">
+            <img className="h-48" src="./CO2-Cable.png" alt="" />
+            <h2 className="text-lg text-left font-normal mb-auto">TMA-2 HD Wireless</h2>
+            <h3 className="text-lg text-left font-bold mb-auto">USD 350</h3>
+          </div>
+          <div className="card flex flex-col items-start w-56 min-w-max mr-4">
+            <img className="h-48" src="./TMA-2-Modular-Headphone.png" alt="" />
+            <h2 className="text-lg text-left font-normal mb-auto">TMA-2 HD Wireless</h2>
+            <h3 className="text-lg text-left font-bold mb-auto">USD 350</h3>
+          </div>
+          <div className="card flex flex-col items-start w-56 min-w-max mr-4">
+            <img className="h-48" src="./CO2-Cable.png" alt="" />
+            <h2 className="text-lg text-left font-normal mb-auto">TMA-2 HD Wireless</h2>
+            <h3 className="text-lg text-left font-bold mb-auto">USD 350</h3>
+          </div>
+          <div className="card flex flex-col items-start w-56 min-w-max mr-4">
+            <img className="h-48" src="./TMA-2-Modular-Headphone.png" alt="" />
+            <h2 className="text-lg text-left font-normal mb-auto">TMA-2 HD Wireless</h2>
+            <h3 className="text-lg text-left font-bold mb-auto">USD 350</h3>
+          </div>
+          <div className="card flex flex-col items-start w-56 min-w-max mr-4">
+            <img className="h-48" src="./CO2-Cable.png" alt="" />
+            <h2 className="text-lg text-left font-normal mb-auto">TMA-2 HD Wireless</h2>
+            <h3 className="text-lg text-left font-bold mb-auto">USD 350</h3>
+          </div>
+          <div className="card flex flex-col items-start w-56 min-w-max mr-4">
+            <img className="h-48" src="./TMA-2-Modular-Headphone.png" alt="" />
+            <h2 className="text-lg text-left font-normal mb-auto">TMA-2 HD Wireless</h2>
+            <h3 className="text-lg text-left font-bold mb-auto">USD 350</h3>
+          </div>
+          <div className="card flex flex-col items-start w-56 min-w-max mr-4">
+            <img className="h-48" src="./CO2-Cable.png" alt="" />
+            <h2 className="text-lg text-left font-normal mb-auto">TMA-2 HD Wireless</h2>
+            <h3 className="text-lg text-left font-bold mb-auto">USD 350</h3>
+          </div>
+          <div className="card flex flex-col items-start w-56 min-w-max mr-4">
+            <img className="h-48" src="./TMA-2-Modular-Headphone.png" alt="" />
+            <h2 className="text-lg text-left font-normal mb-auto">TMA-2 HD Wireless</h2>
+            <h3 className="text-lg text-left font-bold mb-auto">USD 350</h3>
+          </div>
+          <div className="card flex flex-col items-start w-56 min-w-max mr-4">
+            <img className="h-48" src="./CO2-Cable.png" alt="" />
+            <h2 className="text-lg text-left font-normal mb-auto">TMA-2 HD Wireless</h2>
+            <h3 className="text-lg text-left font-bold mb-auto">USD 350</h3>
           </div>
         </div>
       </div>
