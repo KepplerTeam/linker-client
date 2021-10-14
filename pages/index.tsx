3315e9357acf1878cd47448ba8263979a8738666
Luis Alejandro Graterol <luisgraterol97@gmail.com>
2021-10-14 17:55:58 -0400

    Agregar search bar y scroll de productos

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,78 @@ import { GET_USERS } from '../graphql/queries';
 import { User } from '../models/index';
 
 export default function Home() {
-  const { data, loading } = useQuery<{
-    users: User[];
-  }>(GET_USERS, {
-    fetchPolicy: 'network-only',
-  });
+  // const { data, loading } = useQuery<{
+  //   users: User[];
+  // }>(GET_USERS, {
+  //   fetchPolicy: 'network-only',
+  // });
+  // return (
+  //   <div>
+  //     {(loading && <div className="">Hello World</div>) ||
+  //       data?.users.map((user) => (
+  //         <h1>
+  //           username: {user.username} id: {user._id}
+  //         </h1>
+  //       ))}
+  //   </div>
+  // );
+
   return (
-    <div>
-      {(loading && <div className="">Hello World</div>) ||
-        data?.users.map((user) => (
-          <h1>
-            username: {user.username} id: {user._id}
-          </h1>
-        ))}
+    <div className="w-screen h-full p-0">
+      <div className="nav p-3">
+        <a className="flex items-center" href="#">
+          <img className="h-7" src="./icons/menu-variant.svg" alt="Menu" />
+        </a>
+        <a className="mx-auto" href="#">
+          <img className="h-8" src="./logo.svg" alt="Linker" />
+        </a>
+        <a className="flex items-center" href="#">
+          <img className="h-7" src="./profile-pic.png" alt="Perfil" />
+        </a>
+      </div>
+
+
+      <div className="w-full mt-3 p-6">
+        <h4 className="text-lg font-light pb-2">Bienvenido, “username”</h4>
+        <h1 className="text-3xl font-bold">¿Qué desea comprar hoy?</h1>
+        <input className="input-text" type="text" name="search" id="search" placeholder="Buscar Productos"/>
+      </div>
+
+      <div className="card-home">
+        <div className="nav scroll-x mb-4">
+          <button className="nav-item active">Construcci&oacute;n</button>
+          <button className="nav-item">Comida</button>
+          <button className="nav-item">Pl&aacute;stico</button>
+          <button className="nav-item mr-0">Cart&oacute;n</button>
+        </div>
+        <div className="flex scroll-x">
+          <div className="card flex items-center justify-between w-96 min-w-max mr-4">
+            <div className="flex flex-col items-start h-full">
+              <h2 className="text-2xl font-bold mb-auto">TMA-2 <br />Modular <br />Headphone</h2>
+              <a href="#" className="nav-link">
+                Shop Now
+                <img className="ml-2" src="./icons/arrow-right.png" alt=">" />
+              </a>
+            </div>
+            <img className="h-48" src="./TMA-2-Modular-Headphone.png" alt="" />
+          </div>
+          <div className="card flex items-center justify-between w-96 min-w-max mr-4">
+            <div className="flex flex-col items-start h-full">
+              <h2 className="text-2xl font-bold mb-auto">CO2 <br />Cable</h2>
+              <a href="#" className="nav-link">
+                Shop Now
+                <img className="ml-2" src="./icons/arrow-right.png" alt=">" />
+              </a>
+            </div>
+            <img className="h-48" src="./CO2-Cable.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <img className="h-6" src="./logo.svg" alt="Linker" />
+        <h4 className="text-white pb-5">by KepplerTeam</h4>
+        <h4 className="text-sm font-light text-primary-400 pb-2">© Linker 2021. All Rights Reserved</h4>
+      </div>
     </div>
   );
 }
