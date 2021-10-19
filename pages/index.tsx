01f94252ac0fe250280b0612933ecc7f0a0112b3
Sebastian Vivas <sebastian@avilatek.dev>
2021-10-19 11:00:20 -0400

    units dropdown

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,22 +8,6 @@ import CardHome from '../components/home/CardHome';
 import Footer from '../components/common/Footer';
 
 export default function Home() {
-  // const { data, loading } = useQuery<{
-  //   users: User[];
-  // }>(GET_USERS, {
-  //   fetchPolicy: 'network-only',
-  // });
-  // return (
-  //   <div>
-  //     {(loading && <div className="">Hello World</div>) ||
-  //       data?.users.map((user) => (
-  //         <h1>
-  //           username: {user.username} id: {user._id}
-  //         </h1>
-  //       ))}
-  //   </div>
-  // );
-
   return (
     <div className="w-screen h-full p-0">
       <Nav />
