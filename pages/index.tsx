644262e532613253b5f661cae9abedc2c517df85
Carlos Graterol <cagpcd@gmail.com>
2021-11-08 00:54:22 -0400

    Add landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import Nav from '../components/Navbar/Nav';
-import Search from '../components/home/Search';
-import CardHome from '../components/home/CardHome';
+import { useRouter } from 'next/router';
 import Footer from '../components/common/Footer';
 import { GET_PRODUCTS } from '../graphql/queries';
 import { Product } from '../models';
@@ -13,6 +11,7 @@ export default function Home() {
   const [category, setCategory] = React.useState<number>(0);
   const [user] = useUser();
   const [open, setOpen] = React.useState(false);
+  const router = useRouter();
 
   const { data, loading } = useQuery<{
     products: Product[];
@@ -44,14 +43,61 @@ export default function Home() {
         )) || (
           <div className="w-screen h-full p-0">
             <div>
-              <Nav open={open} setOpen={setOpen} />
+              {/* <Nav open={open} setOpen={setOpen} />
               <Search data={featuredProducts?.products} />
               <CardHome
                 allProducts={data?.products}
                 featuredProducts={featuredProducts?.products}
                 category={category}
                 setCategory={setCategory}
-              />
+              /> */}
+              <div className="w-full flex flex-row px-3 pt-3 pb-4 rounded-br-xl rounded-bl-xl bg-gray-200 shadow-2xl">
+                <button
+                  type="button"
+                  className="flex items-center"
+                  // onClick={() => handleOpen()}
+                >
+                  <img className="h-7" src="./icons/menu-variant.svg" alt="Menu" />
+                </button>
+                <a className="mx-auto" href="/">
+                  <img className="h-8" src="./logo.svg" alt="Linker" />
+                </a>
+                <button
+                  type="button"
+                  className="signup flex items-center"
+                  onClick={() => router.push('/login')}
+                >
+                  Log In
+                </button>
+              </div>
+
+              <div className="w-full mb-10 p-6 py-32 justify-center bg-landing-background bg-cover bg-white shadow-2xl">
+                <h2 className="text-3xl font-bold text-center text-white opacity-100 mx-10">La forma más sencilla de comprar y vender</h2>
+                <div className="flex justify-center">
+                  <button
+                    type="button"
+                    className="flex items-center bg-primary-100 text-white font-bold text-xl rounded-xl border-0 px-5 py-3 my-8 opacity-100 shadow-xl hover:bg-primary-600"
+                    onClick={() => router.push('/feed')}
+                  >
+                    Launch Linker
+                  </button>
+                </div>
+              </div>
+
+              <div className="w-full p-6 bg-white rounded-2xl">
+                <h2 className="flex justify-center font-bold text-xl my-10">¿Quiénes somos?</h2>
+                <p className="flex justify-center my-2 mx-6 text-justify text-lg font-medium">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+                </p>
+              </div>
+
+              <div className="w-full p-6 bg-primary-100">
+                <h2 className="flex justify-center font-bold text-xl my-4">Main Features</h2>
+                <p className="flex justify-center my-2 mx-6 text-justify text-lg font-medium">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+                </p>
+              </div>
+
               <Footer />
             </div>
           </div>
